64f4eee4046f2543712df9364fc85f12d0c7ac4c
jinwapp <jinwapp@gmail.com>
2022-11-24 15:03:36 +0900

    add: 221124

---
+++ 2022/11/BJ2231_BoonHaeHap.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2231_BoonHaeHap {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
