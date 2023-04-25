3ac32284708af8ff68e4b307dc1781030b0559af
jinwapp <jinwapp@gmail.com>
2023-04-25 23:17:46 +0900

    completed

--- backjoon/PG_Pocketmon.java
+++ backjoon/PG_Pocketmon.java
@@ -2,20 +2,22 @@ import java.util.HashMap;
 
 public class PG_Pocketmon {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-
+        int[] nums = {3, 3, 3, 2,2,2};
+        System.out.println(solution(nums));
     }
 
-    public int solution(int[] nums) {
+    public static int solution(int[] nums) {
         HashMap<Integer, Integer> hp = new HashMap<>();
+        int count = nums.length/2;
         int res = 0;
         for (int key : nums) {
             hp.put(key, hp.getOrDefault(key, 0) + 1);
         }
         for (Integer i : hp.keySet()) {
-            if (hp.get(i) != 0) {
-
-            }
+            if (res == count) {
+                break;
+            } else res++;
         }
+        return res;
     }
 }
