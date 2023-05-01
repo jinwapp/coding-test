f7d2942eaa5f214adc4d31e6dba55240c551288a
jinwapp <jinwapp@gmail.com>
2023-05-01 22:44:44 +0900

    phoneList

--- programmers/phoneList.java
+++ programmers/phoneList.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class phoneList {
+    public static void main(String[] args) {
+
+        String[] phone_book = {"123","456","789"};
+
+        System.out.println(solution(phone_book));
+
+    }
+
+    public static boolean solution(String[] phone_book) {
+
+        HashSet<String> hs = new HashSet<>();
+
+        for (String key : phone_book) {
+            hs.add(key);
+        }
+
+        for (String phone : phone_book) {
+            for (int j = 1; j < phone.length(); j++) {
+                if (hs.contains(phone.substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 
