315a126eb92aeea6cb76d03465b0baaabf8e5226
jinwapp <jinwapp@gmail.com>
2023-05-11 00:07:10 +0900

    update

--- programmers/bestAlbum.java
+++ programmers/bestAlbum.java
@@ -3,27 +3,20 @@ import java.util.HashMap;
 public class bestAlbum {
     public static void main(String[] args) {
 
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
-
-        System.out.println(solution(genres, plays));
-
-        //[4, 1, 3, 0]
+//        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+//        int[] plays = {500, 600, 150, 800, 2500};
+//
+//        System.out.println(solution(genres, plays));
+//
+//        //[4, 1, 3, 0]
     }
 
     public static int solution(String[] genres, int[] plays) {
 
-        HashMap<String, Integer> hp = new HashMap<>();
-
-        for (int i = 0; i < genres.length; i++) {
-            hp.put(genres[i], hp.getOrDefault(genres[i],0)+plays[i]);
-        }
-
-        for (int value : hp.values()) {
-            int[] sort = new int[100000000];
-            srt
-
-        }
+        /**
+         * 다시 돌아와서 풀기
+         */
+        return 0;
 
     }
 }
