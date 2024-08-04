51aff1c3ed9369edfe76bb4cf1a757f053a5d91f
jinw <jinwapp@gmail.com>
2024-08-05 00:53:41 +0900

    update

---
+++ programmers/PhoneNumberList_240804.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+
+public class PhoneNumberList_240804 {
+
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        Boolean result = solution(phone_book);
+        System.out.println("result = " + result);
+    }
+
+    public static boolean solution(String[] phone_book) {
+        HashSet<String> hs = new HashSet<>();
+        Boolean answer = true;
+
+        for (String key : phone_book) {
+            hs.add(key);
+        }
+
+        System.out.println("hashset = " + hs);
+
+        for (String phone : phone_book) {
+            System.out.println("phone = " + phone);
+            for (int i = 1; i < phone.length(); i++) {
+                if (hs.contains(phone.substring(0, i))) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+/** 내꺼 답안
+ *     public static Boolean solution(String[] phone_book) {
+ *         Arrays.sort(phone_book);
+ *
+ *         Boolean answer = true;
+ *
+ *         for (int i = 0; i < phone_book.length-1; i++) {
+ *             if (phone_book[i + 1].startsWith(phone_book[i])) {
+ *                 answer = false;
+ *                 break;
+ *             }
+ *         }
+ *         return answer;
+ *     }
+ */
